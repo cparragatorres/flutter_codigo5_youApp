5e9e0354e75a76982b49b28d405a1cc6d490ffef
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2022-06-08 20:43:37 -0500

    Init 6

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,5 @@
-
 import 'package:flutter/material.dart';
+import 'package:flutter_codigo5_youapp/ui/general/colors.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -7,8 +7,32 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Hola"),
+      backgroundColor: kBrandPrimaryColor,
+      body: Column(
+        children: [
+          Row(
+            children: [
+              ElevatedButton.icon(
+                onPressed: () {},
+                icon: Icon(Icons.explore_outlined),
+                label: Text(
+                  "Explorar",
+                ),
+                style: ElevatedButton.styleFrom(
+                    elevation: 0,
+                    primary: kBrandSecondaryColor,
+                    padding: const EdgeInsets.symmetric(horizontal: 10.0)),
+              ),
+              const SizedBox(
+                height: 32.0,
+                child: VerticalDivider(
+                  color: Colors.white24,
+                  thickness: 0.9,
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
--- lib/ui/general/colors.dart
+++ lib/ui/general/colors.dart
@@ -1,2 +1,3 @@
 import 'dart:ui';
-const Color kBrandPrimaryColor = Color(0xff241E28);
+const Color kBrandPrimaryColor = Color(0xff212121);
+const Color kBrandSecondaryColor = Color(0xff383838);
