1d1f69fa85e762dfd431ad5a88505f4d2f1464e4
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2022-06-09 19:24:11 -0500

    Servicios 5

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -22,6 +22,7 @@ class _HomePageState extends State<HomePage> {
     // TODO: implement initState
     super.initState();
     getData();
+    _apiService.getChannel("sadasdasd");
   }
 
   getData(){
--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -9,7 +9,7 @@ class APIService{
 
 
   Future<List<VideoModel>> getVideos() async{
-    String _path = "https://youtube.googleapis.com/youtube/v3/search?part=snippet&key=&maxResults=20&regionCode=US";
+    String _path = "https://youtube.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyDcwD_ZOkxDiAgxn8QhG7lUao7afdJPBrE&maxResults=20&regionCode=US";
     Uri _uri = Uri.parse(_path);
     http.Response response = await http.get(_uri);
     if(response.statusCode == 200){
@@ -19,6 +19,17 @@ class APIService{
       return videoModelList;
     }
     return [];
+  }
+
+
+  getChannel(String channelId) async{
+    String _path = "https://youtube.googleapis.com/youtube/v3/channels?part=snippet,brandingSettings,contentDetails,topicDetails&id=UCMnHk6_QgQT1KQkbUlxhTYw&key=";
+    Uri _uri = Uri.parse(_path);
+    http.Response response = await http.get(_uri);
+    if(response.statusCode == 200){
+      Map<String, dynamic> channelMap = json.decode(response.body);
+      print(channelMap);
+    }
   }
 
 
