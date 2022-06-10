9e106dfac73f720ca4c4bd3b952d100d9da46f22
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2022-06-09 22:22:19 -0500

    Video Detail 4

--- lib/pages/video_detail_page.dart
+++ lib/pages/video_detail_page.dart
@@ -1,15 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_codigo5_youapp/ui/general/colors.dart';
 import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
 class VideoDetailPage extends StatefulWidget {
   String videoId;
+
   VideoDetailPage({required this.videoId});
+
   @override
   State<VideoDetailPage> createState() => _VideoDetailPageState();
 }
 
 class _VideoDetailPageState extends State<VideoDetailPage> {
-
   late YoutubePlayerController _controller;
 
   @override
@@ -26,7 +28,6 @@ class _VideoDetailPageState extends State<VideoDetailPage> {
     );
   }
 
-
   @override
   void dispose() {
     // TODO: implement dispose
@@ -36,10 +37,10 @@ class _VideoDetailPageState extends State<VideoDetailPage> {
 
   @override
   Widget build(BuildContext context) {
-
     double height = MediaQuery.of(context).size.height;
 
     return Scaffold(
+      backgroundColor: kBrandPrimaryColor,
       body: Column(
         children: [
           SizedBox(
@@ -65,6 +66,99 @@ class _VideoDetailPageState extends State<VideoDetailPage> {
               // ),
             ),
           ),
+          ListTile(
+            title: Text(
+              "Lorem ipsum dolor sit amet Lorem ipsum dolor sitorem ipsum dolor sit amet",
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+              style:
+                  TextStyle(color: Colors.white, fontSize: 14.0, height: 1.3),
+            ),
+            subtitle: Container(
+              margin: const EdgeInsets.symmetric(vertical: 4.0),
+              child: const Text(
+                "8.8 M de vistas · hace 5 años",
+                style: TextStyle(
+                  color: Colors.white54,
+                  fontSize: 13.0,
+                ),
+              ),
+            ),
+            trailing: const Icon(
+              Icons.keyboard_arrow_down,
+              color: Colors.white,
+            ),
+          ),
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            physics: const BouncingScrollPhysics(),
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  IconDetailVideoWidget(
+                    icon: Icons.thumb_up_alt_outlined,
+                    text: "354 K",
+                  ),
+                  IconDetailVideoWidget(
+                    icon: Icons.thumb_down_alt_outlined,
+                    text: "No me gusta",
+                  ),
+                  IconDetailVideoWidget(
+                    icon: Icons.share,
+                    text: "Compartir",
+                  ),
+                  IconDetailVideoWidget(
+                    icon: Icons.download_rounded,
+                    text: "Descargar",
+                  ),
+                  IconDetailVideoWidget(
+                    icon: Icons.cut,
+                    text: "Recortar",
+                  ),
+                  IconDetailVideoWidget(
+                    icon: Icons.video_settings,
+                    text: "Guardar",
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class IconDetailVideoWidget extends StatelessWidget {
+
+  IconData icon;
+  String text;
+  IconDetailVideoWidget({required this.icon, required this.text,});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(right: 20.0),
+      child: Column(
+        children: [
+          Icon(
+            icon,
+            color: Colors.white,
+            size: 26.0,
+          ),
+          SizedBox(
+            height: 6.0,
+          ),
+          Text(
+            text,
+            style: TextStyle(
+              color: Colors.white60,
+              fontSize: 12.0,
+              fontWeight: FontWeight.w500
+            ),
+          )
         ],
       ),
     );
